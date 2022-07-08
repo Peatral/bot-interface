f2776bea9997b2b36093050289ca39e37d5a0899
Christof Reimers <christof.reimers@outlook.de>
2022-07-08 11:58:12 +0200

    🐛 Fix login link

--- layouts/Default.jsx
+++ layouts/Default.jsx
@@ -6,25 +6,32 @@ import {UserContext} from "@components/context/UserContext";
 import {ThemeContext} from "@components/context/ThemeContext";
 import {UserDetails} from "@components/UserDetails";
 import {Loader} from "@components/Loader";
+import {useRouter} from "next/router";
 
 const {NEXT_PUBLIC_URL} = process.env;
 
 export default function DefaultLayout({children}) {
   const [userContext] = useContext(UserContext);
   const [theme] = useContext(ThemeContext);
+  const Router = useRouter();
   return (
     <MantineProvider theme={{colorScheme: theme}}>
       <AppShell
         header={
           <HeaderBar>
             <HeaderLink label="Home" link={NEXT_PUBLIC_URL} external />
-            <HeaderLink label="Presentations" link={`${NEXT_PUBLIC_URL}/presentations`} external />
+            <HeaderLink
+              label="Presentations"
+              link={`${NEXT_PUBLIC_URL}/presentations`}
+              external
+            />
             <HeaderLink label="Bot" link="/" />
             {userContext.details === null ? (
               <HeaderLink
                 label="Login"
-                link={`/api/users/auth/discord`}
+                link={`${NEXT_PUBLIC_URL}${Router.basePath}/api/users/auth/discord`}
                 style={{marginLeft: "auto"}}
+                external
               />
             ) : !userContext.details ? (
               <HeaderItem style={{marginLeft: "auto"}}>
