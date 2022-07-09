94e2f2dc13cb5e8a7a1d9dd97d740bb7060cf144
uuigx <uuigx@student.kit.edu>
2022-07-09 23:32:05 +0200

    🚑 Stuff returns connection directly

--- utils/dbutils.js
+++ utils/dbutils.js
@@ -2,7 +2,7 @@ import dbConnect from "./connectdb";
 import {respondWithInternalServerError} from "./apiutil";
 
 export async function checkDBConnection(req, res, next) {
-  const {conn} = await dbConnect();
+  const conn = await dbConnect();
 
   if (!conn) {
     return respondWithInternalServerError(res, "Could not connect to database");
